83d4d82f0e0f5b59d7683f25348c849775b908e4
AllanConexa <allan.capistrano@team.conexa.app>
2025-10-13 13:49:46 -0300

    Adaptando a classe CTestCase para funcionar com a versão 11 do PHPUnit

--- framework/test/CTestCase.php
+++ framework/test/CTestCase.php
@@ -9,11 +9,29 @@
  */
 
 if(!class_exists('PHPUnit_Runner_Version')) {
-	require_once('PHPUnit/Runner/Version.php');
-	require_once('PHPUnit/Util/Filesystem.php'); // workaround for PHPUnit <= 3.6.11
+    if (file_exists(__DIR__ . '/../../vendor/phpunit/phpunit/src/Runner/Version.php')) {
+        require_once(__DIR__ . '/../../vendor/phpunit/phpunit/src/Runner/Version.php');
+    } elseif (stream_resolve_include_path('PHPUnit/Runner/Version.php')) {
+        require_once('PHPUnit/Runner/Version.php');
+    }
+
+    if (file_exists(__DIR__ . '/../../vendor/phpunit/phpunit/src/Util/Filesystem.php')) {
+        require_once(__DIR__ . '/../../vendor/phpunit/phpunit/src/Util/Filesystem.php');
+    } elseif (stream_resolve_include_path('PHPUnit/Runner/Version.php')) {
+        require_once('PHPUnit/Runner/Version.php');
+    }
 
 	spl_autoload_unregister(array('YiiBase','autoload'));
-	require_once('PHPUnit/Autoload.php');
+
+    if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+        require_once(__DIR__ . '/../../vendor/autoload.php');
+    } elseif (stream_resolve_include_path('PHPUnit/Autoload.php')) {
+        // Fallback para PHPUnit antigo
+        require_once('PHPUnit/Autoload.php');
+    } else {
+        trigger_error('Nenhum autoloader PHPUnit encontrado (Composer ou legado).', E_USER_WARNING);
+    }
+
 	spl_autoload_register(array('YiiBase','autoload')); // put yii's autoloader at the end
 
 	if (in_array('phpunit_autoload', spl_autoload_functions())) { // PHPUnit >= 3.7 'phpunit_autoload' was obsoleted
